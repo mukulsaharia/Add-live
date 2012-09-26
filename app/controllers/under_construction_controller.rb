615e9e189ba1df5418826a341d7c460a017e56d2
mukul saharia <mukul.saharia@gmail.com>
2012-09-26 22:38:04 +0530

    Uder construction git

---
+++ app/controllers/under_construction_controller.rb
@@ -0,0 +1,7 @@
+class UnderConstructionController < ApplicationController
+	
+	def index
+	
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddLive::Application.config.secret_token = '84bd38e7b82ea5402d3c457b9e3d1bc11a9d302d59041617e7011aed5fc0f855c8c2be20b07f244e1469a41d6bcc644ce7954c276ab79d9434d6344db2c9b1aa'
