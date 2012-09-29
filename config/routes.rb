a69abfc9b3df4fd0db7ac7339fcf3847e38068bc
mukul saharia <mukul.saharia@gmail.com>
2012-09-30 00:35:12 +0530

    basic home page structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ AddLive::Application.routes.draw do
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  root :to => 'under_construction#index'
+  root :to => 'staticpage#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
