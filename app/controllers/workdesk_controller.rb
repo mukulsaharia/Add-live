e66fb5de615064fdb2f82e97998b33bb821d1f37
mukul saharia <mukul.saharia@gmail.com>
2013-02-13 14:14:51 +0530

    removed refer list

--- app/controllers/workdesk_controller.rb
+++ app/controllers/workdesk_controller.rb
@@ -2,10 +2,11 @@ class WorkdeskController < ApplicationController
 	before_filter :authenticate_user!
 	$balance_points
 		@refer_list=User.where("sponserID=?",current_user.username)
+		@ads=Ads.find(:all, :order => "RANDOM()", :limit => 10)
 	def index
 		@count=1
 		@user=current_user.fname
-		@ads=Ads.find(:all, :order => "RANDOM()", :limit => 10)
+		#@ads=Ads.find(:all, :order => "RANDOM()", :limit => 10)
 		#@refer_list=User.where("sponserID=?",current_user.username)
 		@pins= Pin.where("frenchisename=?",current_user.username)
 		@total_ads_points=Points.where( :user_id => current_user.id, :Referal => false).sum :points
