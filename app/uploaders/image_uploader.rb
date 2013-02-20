dcbfc82e6fff64d626558d61ca7d489b302ebeba
mukul saharia <mukul.saharia@gmail.com>
2013-02-20 13:37:51 +0530

    changes in uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
   
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root}/assets/uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
